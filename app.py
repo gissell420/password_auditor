9b5a92a8399a14230850255000831e6e8f19134f
Gabriel Rivera <rivergab@kean.edu>
2026-04-16 23:12:07 -0400

    Batch Plaintext Processing
    Menu functionality added to facilitate either single or batch plaintext password processing. Outputs batch data to terminal.

--- app.py
+++ app.py
@@ -6,15 +6,53 @@ from pwned_api import check_pwned_api
 def main():
     print("Password Strength Analyzer & Policy Auditor")
     print("------------------------------------------")
+    while True:
+        print("Options: Single (1) | Batch (2) | Exit (0)")
+        try:
+            option = int(input("Choice: "))
+            match option:
+                case 1:
+                    single()
+                case 2:
+                    batch()
+                case _:
+                    break
 
+        except ValueError:
+            print("Enter only the number for an option.")
+    print("END") #Remove in final
+        
+
+
+def single():
     password = input("Enter a password to analyze: ")
 
     strength_result = analyze_password_strength(password)
     policy_result = check_password_policy(password)
-
     leaks = check_pwned_api(password)
 
     print_report(strength_result, policy_result, leaks)
+
+        
+def batch(): #Can only use zxcvbn on plaintext passwords. Can only use JtR on hashed passwords.
+    location = input("Enter the file location:")
+    try:
+        password_file = open(location,'r')
+    except FileNotFoundError:
+        print("File not found.")
+        return
+    pass_list = [line.strip() for line in password_file]
+    i = 1;print('\n===== COMPLIANCE REPORT =====')
+    for password in pass_list:
+        strength_result = analyze_password_strength(password)
+        policy_result = check_password_policy(password)
+        leaks = check_pwned_api(password)
+        print(f'{i}:')
+        i+=1
+        #Possibly output to txt file in future.
+        print_report(strength_result,policy_result,leaks,batch=True)
+        
+
 
 if __name__ == "__main__":
     main()
--- report.py
+++ report.py
@@ -1,32 +1,47 @@
-def print_report(strength_result, policy_result, leaks):
-    print("\n===== PASSWORD ANALYSIS REPORT =====")
-    print(f"Strength Score: {strength_result['score']} / 4")
-    print(f"Estimated Guesses: {strength_result['guesses']}")
+def print_report(strength_result, policy_result, leaks, batch: bool = False):
+    if not batch: #Single
+        print("\n===== PASSWORD ANALYSIS REPORT =====")
+        print(f"Strength Score: {strength_result['score']} / 4")
+        print(f"Estimated Guesses: {strength_result['guesses']}")
 
-    print("\nCrack Time Estimates:")
-    for attack_type, estimate in strength_result["crack_times_display"].items():
-        print(f"- {attack_type}: {estimate}")
+        print("\nCrack Time Estimates:")
+        for attack_type, estimate in strength_result["crack_times_display"].items():
+            print(f"- {attack_type}: {estimate}")
 
-    print("\nPolicy Checks:")
-    for check, passed in policy_result.items():
-        status = "PASS" if passed else "FAIL"
-        print(f"- {check}: {status}")
+        print("\nPolicy Checks:")
+        for check, passed in policy_result.items():
+            status = "PASS" if passed else "FAIL"
+            print(f"- {check}: {status}")
 
-    print("\nData Breach Check:")
-    if int(leaks) > 0:
-        print(f"❌ CAUTION: This password was seen in {leaks} data leaks!")
-        print("Suggestion: Change this password immediately on all accounts.")
-    else:
-        print("✅ Safe! This password has not been found in known breaches.")
+        print("\nData Breach Check:")
+        if int(leaks) > 0:
+            print(f"❌ CAUTION: This password was seen in {leaks} data leaks!")
+            print("Suggestion: Change this password immediately on all accounts.")
+        else:
+            print("✅ Safe! This password has not been found in known breaches.")
 
-    feedback = strength_result.get("feedback", {})
-    warning = feedback.get("warning")
-    suggestions = feedback.get("suggestions", [])
+        feedback = strength_result.get("feedback", {})
+        warning = feedback.get("warning")
+        suggestions = feedback.get("suggestions", [])
 
-    if warning or suggestions:
-        print("\nFeedback:")
-        if warning:
-            print(f"- Warning: {warning}")
-        for suggestion in suggestions:
-            print(f"- Suggestion: {suggestion}")
+        if warning or suggestions:
+            print("\nFeedback:")
+            if warning:
+                print(f"- Warning: {warning}")
+            for suggestion in suggestions:
+                print(f"- Suggestion: {suggestion}")
+
+
+    else: #Batch (Removes headers and adds tabs to front. 
+            #Can probably integrate into the prior stuff with more conditionals/ternary but opted not to.)
+        print(f"\tStrength Score: {strength_result['score']} / 4")
+        print(f"\tEstimated Guesses: {strength_result['guesses']}")
+        for attack_type, estimate in strength_result["crack_times_display"].items():
+            print(f"\t- {attack_type}: {estimate}")
+        print(f'\tOverall Policy Compliance: {policy_result["Overall policy compliant"]}')
+        if int(leaks) > 0:
+            print(f"\t❌ CAUTION: This password was seen in {leaks} data leaks!")
+            print("\tSuggestion: Change this password immediately on all accounts.")
+        else:
+            print("\t✅ Safe! This password has not been found in known breaches.")
 
